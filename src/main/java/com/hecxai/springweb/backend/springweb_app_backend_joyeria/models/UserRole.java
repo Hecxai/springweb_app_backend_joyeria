c2f71e9ab262f7a4ee98d6be27017e5c01fe8a30
Soy Hecxai <mariohecxaiv@gmail.com>
2025-10-13 17:50:09 -0600

    archivo modificado y no subido inicialmente a github

--- src/main/java/com/hecxai/springweb/backend/springweb_app_backend_joyeria/models/UserRole.java
+++ src/main/java/com/hecxai/springweb/backend/springweb_app_backend_joyeria/models/UserRole.java
@@ -1,5 +1,53 @@
 package com.hecxai.springweb.backend.springweb_app_backend_joyeria.models;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user_role")
 public class UserRole {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_role_id")
+    private Long userRoleId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "role_id", nullable = false)
+    private Role role;
+
+    public UserRole() {
+    }
+
+    public UserRole(User user, Role role) {
+        this.user = user;
+        this.role = role;
+    }
+
+    public Long getUserRoleId() {
+        return userRoleId;
+    }
+
+    public void setUserRoleId(Long userRoleId) {
+        this.userRoleId = userRoleId;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
 
 }
